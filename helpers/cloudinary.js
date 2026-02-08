ffcb79f0e1d4c8f83b4942d87be47491105141d2
Edmar Eustaquio <edmareustaquio@Edmars-MacBook-Air.local>
2026-02-08 08:27:26 +0800

    update cloudinary api

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -36,10 +36,13 @@ const uploadImageUri = async (uri) => {
   };
 
   formData.append("file", param);
-  formData.append("upload_preset", "upload_file");
+  formData.append("upload_preset", "images");
+  // formData.append("upload_preset", "upload_file");
 
+  // 741351497561832
   const res = await axios.post(
-    "https://api.cloudinary.com/v1_1/diwwrxy8b/image/upload",
+    "https://api.cloudinary.com/v1_1/dnvnkh2md/image/upload",
+    // "https://api.cloudinary.com/v1_1/diwwrxy8b/image/upload",
     formData,
     {
       headers: { "Content-Type": "multipart/form-data" },
